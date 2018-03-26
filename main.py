4092c9b04cb42867861f472b0b2dcb69bde94c96
Jesse Williams <jesse_1974@att.net>
2018-03-25 21:25:58 -0500

    added caesar encryption function

--- main.py
+++ main.py
@@ -1,10 +1,62 @@
-from flask import Flask
+from flask import Flask, request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+
+            }
+
+        </style>
+    </head>
+    <body>
+            <form method='post'>
+                <label> Rotate by:
+                    <input type="text" name="rot" id="rot" value="0">
+                </label>
+                <textarea name="text"></textarea>
+                <input type="submit" value="Submit Query"/>
+            
+            
+            </form>
+    </body>
+</html>"""
+    
+@app.route("/", methods=['POST'])
+def encrypt():
+    #convert_rot = int(rot)
+    rot_int = request.form['rot']
+    text_en = request.form['text']
+    content = rotate_string(text_en, int(rot_int))
+    return """
+    
+    <h1>""" + content + """</h1>
+    
+    """
+    
+
+
+
 @app.route("/")
 def index():
-    return "Hello World"
+    return form 
 
 app.run()
